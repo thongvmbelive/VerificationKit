bb89a9ed0fd7866215e8507b777fc45c0efb181a
Thi <t@thi.im>
2019-05-05 16:25:54 +0900

    Swift 5 and Xcode recommended settings

--- Samples/Samples/AppDelegate.swift
+++ Samples/Samples/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let credentialsProvider = AWSCognitoCredentialsProvider(regionType:REGION_TYPE, identityPoolId:IDENTITY_POOL_ID)
         let configuration = AWSServiceConfiguration(region: REGION_TYPE, credentialsProvider: credentialsProvider)
         AWSServiceManager.default().defaultServiceConfiguration = configuration
